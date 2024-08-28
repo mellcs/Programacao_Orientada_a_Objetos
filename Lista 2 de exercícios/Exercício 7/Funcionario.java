f47b778453998db359d2c8cb0b2363fa4aa81f22
mell <126731231+mellcs@users.noreply.github.com>
2024-08-28 20:18:29 -0300

    Create Funcionario.java

---
+++ Lista 2 de exercícios/Exercício 7/Funcionario.java
@@ -0,0 +1,10 @@
+public class Funcionario {
+    String nome;
+    double salario;
+
+    // Construtor para inicializar os atributos do funcionário
+    public Funcionario(String nome, double salario) {
+        this.nome = nome;
+        this.salario = salario;
+    }
+}
