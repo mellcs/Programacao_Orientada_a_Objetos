4f0d67a63c9b396377b85e78aafac7c232a88bd9
mell <126731231+mellcs@users.noreply.github.com>
2024-08-27 21:46:01 -0300

    Create Livro.java

---
+++ Lista 2 de exercícios/Exercício 4/Livro.java
@@ -0,0 +1,12 @@
+public class Livro {
+    String titulo;
+    String autor;
+    int anoPublicacao;
+
+    // Construtor para inicializar os atributos do livro
+    public Livro(String titulo, String autor, int anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+}
