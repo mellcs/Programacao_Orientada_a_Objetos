cba6157a1aded41741cd2ad2174d6f9e2ad06340
mell <126731231+mellcs@users.noreply.github.com>
2024-09-04 21:38:45 -0300

    Update Carro.java

--- Exercícios 04.09/exercício_01/Carro.java
+++ Exercícios 04.09/exercício_01/Carro.java
@@ -1,1 +1,52 @@
-mimi
+public class Carro {
+
+    private String marca;
+    private String modelo;
+    private int ano;
+    private double preco;
+
+    public Carro(String marca, String modelo, int ano, double preco) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        setPreco(preco); 
+    }
+  
+//get e set
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        if (preco >= 0) {
+            this.preco = preco;
+        } else {
+            System.out.println("O preço não pode ser negativo.");
+        }
+    }
+}
