7f7b6a5bff650cdf21a1c172cd40216151b892d0
mell <126731231+mellcs@users.noreply.github.com>
2024-09-11 21:47:51 -0300

    Create Motor.java

---
+++ Agregação e Composição/exercício 01/Motor.java
@@ -0,0 +1,11 @@
+//composição
+public class Motor {// definição da classe
+    private int potencia;// defninição da variável potencia
+    private String combustivel;// definição da variável combustível
+
+    public Motor (int potencia, String combustivel) {
+        this.potencia = potencia;
+        this.combustivel = combustivel;
+    }
+    
+}
