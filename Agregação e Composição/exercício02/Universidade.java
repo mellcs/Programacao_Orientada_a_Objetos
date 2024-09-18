3555fe68771c2f246edccfe62e1af3d459ed946f
mell <126731231+mellcs@users.noreply.github.com>
2024-09-18 20:21:47 -0300

    Create Universidade.java
    não tá pronto faltam dois métodos

---
+++ Agregação e Composição/exercício02/Universidade.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Universidade{
+    private List<Professores> professores;
+    
+    public Universidade(){//não vai receber nada, só vai fazer algo novo.
+        this.professores = new ArrayList<>();//referencia o professores e gruda na lista.
+    }
+    
+    public void adicionarProfessor(Professor professor){
+        professores.add(professor);
+    }
+    
+    public void listarProfessores
+    
+    
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
