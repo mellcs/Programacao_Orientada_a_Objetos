790f5591d073a981fd2d08fb71c788ad8ac531a9
mell <126731231+mellcs@users.noreply.github.com>
2024-09-18 15:50:36 -0300

    Update Carro.java
    yepeeee

--- Agregação e Composição/exercício 01/Carro.java
+++ Agregação e Composição/exercício 01/Carro.java
@@ -1,10 +1,35 @@
-//composição
+//Em seguida, crie uma classe Carro que tenha um motor como parte integrante. No construtor da classe Carro, 
+//instancie o motor. Implemente métodos para exibir informações sobre o carro e seu motor.
+ 
 public class Carro {
-    private String nomeCarro;
-    private Motor Motor;
-    public Carro (int potencia, String combustivel, String nomeCarro) {
-
-    this.Motor = new Motor(potencia, combustivel);
-    this.nomeCarro = nomeCarro;
+    private Motor motor;//chama o outro e gruda ele aqui
+    
+    //construtor não recebe atribuição de tipo de dado pq é uma ferramenta não uma coisa.
+    public Carro (int potencia, String tipoCombustivel){
+    //chamamos a outra classe antes, então dá pra usar direto os troços dele q nem em python.    
+        this.motor = new Motor(potencia, tipoCombustivel);//usa o objeto pequenino, gruda o motor aos atributos.
     }
-}
+    
+    public Motor getMotor(){//O tipo é a outra classe, pq vem de la
+        return motor;//pequenino
+    }
+    
+    public void SetMotor(Motor motor){//a outra classe foi instanciada primeiro assim, então esses são seus "atributos".
+        this.motor = motor;//pequeninos
+    }
+    
+    public void mostrarInfo(){
+        System.out.println("Informações do carro: ");
+        if (motor != null){
+            motor.exibirInfo();
+        }else{
+            System.out.println("Motor não definido.");
+        }
+    }
+    
+    public static void main (String[] args){
+        Carro carro = new Carro(130, "Diesel");
+        
+        carro.mostrarInfo();
+    }
+ }
