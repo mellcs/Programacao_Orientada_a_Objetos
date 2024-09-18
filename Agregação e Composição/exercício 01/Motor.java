f26f6f39444788e2d20d33c9bf39c4a2885f3697
mell <126731231+mellcs@users.noreply.github.com>
2024-09-18 15:30:10 -0300

    Update Motor.java
    refiz sozinha (chat corrigiu) amoo

--- Agregação e Composição/exercício 01/Motor.java
+++ Agregação e Composição/exercício 01/Motor.java
@@ -1,11 +1,35 @@
-//composição
-public class Motor {// definição da classe
-    private int potencia;// defninição da variável potencia
-    private String combustivel;// definição da variável combustível
-
-    public Motor (int potencia, String combustivel) {
-        this.potencia = potencia;
-        this.combustivel = combustivel;
+public class Motor {
+    private int potencia;
+    private String tipoCombustivel;
+    
+    public Motor (int potencia, String tipoCombustivel) { //construtor pra inicializar os atributos (precisa sempre?)
+        this.potencia = potencia;//tirar do privado e poder usar
+        this.tipoCombustivel = tipoCombustivel;//tirar do privado e poder usar
     }
     
+    public int getPotencia() {//o nome do atributo em letra maiúscula depois do get.
+        return potencia;//só pega o bgl
+    }
+    
+    public void setPotencia(int potencia) {
+        this.potencia = potencia;//declara
+    }
+    
+    public String getTipoCombustivel() {
+        return tipoCombustivel;
+    }
+    
+    public void setTipoCombustivel(String tipoCombustivel) {//set nao tem tipo primitivo, direto void, nao retorna nada
+        this.tipoCombustivel = tipoCombustivel;
+    }
+    
+    public void exibirInfo() {//nao retorna nada, void
+        System.out.println("Potência: " + potencia);
+        System.out.println("Combustível usado: " + tipoCombustivel);
+    }
+    
+    public static void main(String[] args) {
+        Motor motor = new Motor(190, "Aditivada");//cria o objeto e atribui valores
+        motor.exibirInfo();//mostra tudor
+    }
 }
