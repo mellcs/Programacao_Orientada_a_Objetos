330a3b30437417f20cda320fe64b43b3da960988
mell <126731231+mellcs@users.noreply.github.com>
2024-09-18 21:46:28 -0300

    Create Veiculo.java

---
+++ Exercícios de fixação/Exercício02/Meu_método/Veiculo.java
@@ -0,0 +1,11 @@
+public class Veiculo {
+    public String modelo;
+    public String placa;
+    public int capacidade;
+
+    public Veiculo(String modelo, String placa, int capacidade) {
+        this.modelo = modelo;
+        this.placa = placa;
+        this.capacidade = capacidade;
+    }
+}
