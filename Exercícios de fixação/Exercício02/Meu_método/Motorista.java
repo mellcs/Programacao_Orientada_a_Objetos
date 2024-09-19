5a56e35b4647f0199acac873f7366c50e7dfad68
mell <126731231+mellcs@users.noreply.github.com>
2024-09-18 21:46:47 -0300

    Create Motorista.java

---
+++ Exercícios de fixação/Exercício02/Meu_método/Motorista.java
@@ -0,0 +1,9 @@
+public class Motorista {
+    public String nome;
+    public String cnh;
+
+    public Motorista(String nome, String cnh) {
+        this.nome = nome;
+        this.cnh = cnh;
+    }
+}
