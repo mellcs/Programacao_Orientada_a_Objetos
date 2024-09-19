823bf3a2b48d813931a11e5b672dca2e5d60b455
mell <126731231+mellcs@users.noreply.github.com>
2024-09-18 21:49:00 -0300

    Create V.java

---
+++ Exercícios de fixação/Exercício02/Meu_método/Método_chat/V.java
@@ -0,0 +1,23 @@
+public class V {
+    private String mod;
+    private String pl;
+    private int cap;
+
+    public V(String mod, String pl, int cap) {
+        this.mod = mod;
+        this.pl = pl;
+        this.cap = cap;
+    }
+
+    public String getPl() {
+        return pl;
+    }
+
+    public String getMod() {
+        return mod;
+    }
+
+    public int getCap() {
+        return cap;
+    }
+}
