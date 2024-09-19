af13cc2520b5c6523ffba889abcd72ce0802d14e
mell <126731231+mellcs@users.noreply.github.com>
2024-09-18 21:37:47 -0300

    Create E.java

---
+++ Exercícios de fixação/Exercício01/Método_chat/E.java
@@ -0,0 +1,31 @@
+import java.util.Date;
+
+public class E {
+    L l;
+    U u;
+    Date de;
+    Date dd;
+
+    public E(L l, U u, Date de, Date dd) {
+        this.l = l;
+        this.u = u;
+        this.de = de;
+        this.dd = dd;
+    }
+
+    public L getL() {
+        return l;
+    }
+
+    public U getU() {
+        return u;
+    }
+
+    public Date getDe() {
+        return de;
+    }
+
+    public Date getDd() {
+        return dd;
+    }
+}
