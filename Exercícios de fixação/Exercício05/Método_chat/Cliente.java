7041d8a30d153329e02ee2c1b913e12fa68ea4eb
mell <126731231+mellcs@users.noreply.github.com>
2024-09-19 13:56:15 -0300

    Create Cliente.java

---
+++ Exercícios de fixação/Exercício05/Método_chat/Cliente.java
@@ -0,0 +1,23 @@
+public class Cliente {
+    private String n;
+    private String e;
+    private String mail;
+
+    public Cliente(String n, String e, String mail) {
+        this.n = n;
+        this.e = e;
+        this.mail = mail;
+    }
+
+    public String getN() {
+        return n;
+    }
+
+    public String getE() {
+        return e;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+}
