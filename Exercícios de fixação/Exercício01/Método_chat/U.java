c936de44a7f0411b89f61c86e19012d63fc4a36e
mell <126731231+mellcs@users.noreply.github.com>
2024-09-18 21:37:28 -0300

    Create U.java

---
+++ Exercícios de fixação/Exercício01/Método_chat/U.java
@@ -0,0 +1,17 @@
+public class U {
+    String n;
+    String c;
+
+    public U(String n, String c) {
+        this.n = n;
+        this.c = c;
+    }
+
+    public String getN() {
+        return n;
+    }
+
+    public String getC() {
+        return c;
+    }
+}
