5eb405c47e54e8d4d003748497be159596e2e630
mell <126731231+mellcs@users.noreply.github.com>
2024-09-19 13:52:30 -0300

    Create Cliente.java

---
+++ Exercícios de fixação/Exercício05/Meu_método/Cliente.java
@@ -0,0 +1,23 @@
+public class Cliente {
+    private String nome;
+    private String endereco;
+    private String email;
+
+    public Cliente(String nome, String endereco, String email) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
