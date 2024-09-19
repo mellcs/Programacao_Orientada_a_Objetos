550f7f9d9d6931a2d70d344757854574afee906f
mell <126731231+mellcs@users.noreply.github.com>
2024-09-18 21:49:13 -0300

    Create M.java

---
+++ Exercícios de fixação/Exercício02/Meu_método/Método_chat/M.java
@@ -0,0 +1,17 @@
+public class M {
+    private String nm;
+    private String c;
+
+    public M(String nm, String c) {
+        this.nm = nm;
+        this.c = c;
+    }
+
+    public String getNm() {
+        return nm;
+    }
+
+    public String getC() {
+        return c;
+    }
+}
