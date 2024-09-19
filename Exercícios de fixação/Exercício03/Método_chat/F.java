d0dd461dc5f5413162cb314dc3beac636b0d9d6d
mell <126731231+mellcs@users.noreply.github.com>
2024-09-19 13:40:01 -0300

    Create F.java

---
+++ Exercícios de fixação/Exercício03/Método_chat/F.java
@@ -0,0 +1,17 @@
+public class F {
+    private String n;
+    private String c;
+
+    public F(String n, String c) {
+        this.n = n;
+        this.c = c;
+    }
+
+    public String getN() {
+        return n;
+    }
+
+    public String getC() {
+        return c;
+    }
+}
