45e883faa833b971f030e713be2cd6e21c016a00
mell <126731231+mellcs@users.noreply.github.com>
2024-10-02 15:28:32 -0300

    Create Veiculo.java

---
+++ Herança/Exercício08/Veiculo.java
@@ -0,0 +1,11 @@
+//Crie uma superclasse Veiculo com o atributo capacidadeTanque. 
+//Crie uma subclasse Carro que tenha um método exibirCapacidadeTanque(), 
+//o qual imprime o valor do atributo da superclasse. Instancie um objeto da subclasse e utilize o método.
+
+public class Veiculo {
+	protected String capacidadeTanque;
+	
+	public Veiculo (String capacidadeTanque){
+	    this.capacidadeTanque = capacidadeTanque;
+	}
+}
