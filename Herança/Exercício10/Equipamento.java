ca3124e9b60f319fb79793fb927b6ecff878d3a8
mell <126731231+mellcs@users.noreply.github.com>
2024-10-02 15:42:22 -0300

    Create Equipamento.java

---
+++ Herança/Exercício10/Equipamento.java
@@ -0,0 +1,9 @@
+//Crie uma superclasse Equipamento com um método ligar(). Crie uma subclasse Computador e, 
+//sem sobrescrever o método, crie um objeto da subclasse e chame o método ligar() da superclasse para ativar o equipamento.
+
+public class Equipamento {
+	
+	public void ligar(){
+	    System.out.println("Equipamento ligado.");
+	}
+}
