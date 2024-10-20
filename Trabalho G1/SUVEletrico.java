4d35f4d6c5d3e7b21009f90ac730bf324ddd0513
mell <126731231+mellcs@users.noreply.github.com>
2024-10-20 19:20:39 -0300

    Create SUVEletrico.java
    another one

---
+++ Trabalho G1/SUVEletrico.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SUVEletrico extends veiculosEletricos {
+
+    public SUVEletrico(int id_num, String marca, String modelo, int ano_fab, double cap_total_bat, double aut_max) {
+        super(id_num, marca, modelo, ano_fab, cap_total_bat, aut_max);
+    }
+
+    public static void cadastrar() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Cadastro de SUV Elétrico:");
+        veiculosEletricos.addCarro();  // Utiliza o método da classe mãe
+    }
+}
