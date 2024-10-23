2bf0187313b8db058fe6dfe8847901e23b43b38d
mell <126731231+mellcs@users.noreply.github.com>
2024-10-23 15:43:14 -0300

    Update Main.java
    alterado para incorporar novas funções

--- Trabalho G1/Main.java
+++ Trabalho G1/Main.java
@@ -14,6 +14,8 @@ public class Main {
             System.out.println("5 - Listar motoristas.");
             System.out.println("6 - Cadastrar eletroposto.");
             System.out.println("7 - Listar eletropostos.");
+            System.out.println("8 - Registrar recarga.");
+            System.out.println("9 - Histórico de recargas.");
             System.out.println("0 - Sair.");
             System.out.print("O que você gostaria de fazer: ");
             opc = scanner.nextInt();
@@ -39,6 +41,17 @@ public class Main {
                     break;
                 case 7:
                     Eletropostos.exibirPosto(); 
+                    break;
+                case 8:
+                    CarregamentoBaterias.selecionarVeiculo();
+                    break;
+                case 9:
+                    if (!veiculosEletricos.frota.isEmpty()) {
+                        CarregamentoBaterias carregamento = new CarregamentoBaterias(veiculosEletricos.frota.get(0)); // Usa o primeiro veículo ou peça ao usuário para escolher
+                        carregamento.consultarHistoricoRecargas(); 
+                    } else {
+                        System.out.println("Nenhum veículo cadastrado para consultar histórico.");
+                    }
                     break;
                 case 0:
                     System.out.println("Saindo do sistema...");
