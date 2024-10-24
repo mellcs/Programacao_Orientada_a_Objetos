b217abf96b7b7eacd7dd886efac2db3bcfe7d58d
mell <126731231+mellcs@users.noreply.github.com>
2024-10-24 14:48:25 -0300

    Update CarroSedan.java

--- Trabalho G1/CarroSedan.java
+++ Trabalho G1/CarroSedan.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class CarroSedan extends veiculosEletricos {
+public class CarroSedan extends VeiculosEletricos {
 
     public CarroSedan(int id_num, String marca, String modelo, int ano_fab, double cap_total_bat, double aut_max) {
         super(id_num, marca, modelo, ano_fab, cap_total_bat, aut_max);
@@ -10,6 +10,6 @@ public class CarroSedan extends veiculosEletricos {
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Cadastro de Carro Sedan:");
-        veiculosEletricos.addCarro();  // Utiliza o método da classe mãe
+        VeiculosEletricos.addCarro();  // Utiliza o método da classe mãe
     }
 }
