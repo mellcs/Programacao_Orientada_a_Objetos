f5dee4926932ae05fd117b61180728fb55aa2571
Bruno Ilha Carneiro <126731354+Brunoilha@users.noreply.github.com>
2024-11-03 20:30:21 -0300

    Update CarroCompacto.java

--- Trabalho G1/CarroCompacto.java
+++ Trabalho G1/CarroCompacto.java
@@ -9,6 +9,6 @@ public class CarroCompacto extends VeiculosEletricos {
     }
 
     public void cadastrar(List<VeiculosEletricos> frota) {
-        addCarro(frota);  
+        addCarro(frota,this);  
     }
 }
